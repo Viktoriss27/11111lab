11476c391b29df3d8d1eac4a18f1031c12aa1e6f
VIktoriss27 <derkachv27@gmail.com>
2026-06-05 14:08:17 +0300

    Добавлен класс NoteManager

---
+++ WindowsFormsApp6/NoteManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp6
+{
+    public class NoteManager
+    {
+        public List<Note> Notes { get; private set; }
+
+        public NoteManager()
+        {
+            Notes = new List<Note>();
+            LoadNotes();
+        }
+
+        public void AddNote(Note note)
+        {
+            if (note == null)
+            {
+                throw new ArgumentNullException(nameof(note));
+            }
+            Notes.Add(note);
+            SaveNotes();
+        }
+
+        public void RemoveNote(Note note)
+        {
+            if (note == null)
+            {
+                throw new ArgumentNullException(nameof(note));
+            }
+            Notes.Remove(note);
+            SaveNotes();
+        }
+
+        private void SaveNotes()
+        {
+            File.WriteAllLines("notes.txt", Notes.Select(n => $"{n.Title}|{n.Content}|{n.Date.ToString("yyyy-MM-dd HH:mm:ss")}"));
+        }
+
+        private void LoadNotes()
+        {
+            if (File.Exists("notes.txt"))
+            {
+                var lines = File.ReadAllLines("notes.txt");
+                foreach (var line in lines)
+                {
+                    var parts = line.Split('|');
+                    if (parts.Length == 3)
+                    {
+                        DateTime date;
+                        if (DateTime.TryParse(parts[2], out date))
+                        {
+                            Notes.Add(new Note(parts[0], parts[1], date));
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
